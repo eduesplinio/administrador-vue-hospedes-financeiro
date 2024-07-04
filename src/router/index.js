1c1c1d92e1dce32a61f58999acaca6afca78d72b
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2024-07-04 00:40:32 +0000

    define estruturação do projeto

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+// src/router/index.js
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import HospedesView from '@/views/HospedesView.vue';
+import FinancasView from '@/views/FinancasView.vue';
+import PagamentosView from '@/views/PagamentosView.vue';
+import LembretesView from '@/views/LembretesView.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/hospedes', name: 'Hospedes', component: HospedesView },
+  { path: '/financas', name: 'Financas', component: FinancasView },
+  { path: '/pagamentos', name: 'Pagamentos', component: PagamentosView },
+  { path: '/lembretes', name: 'Lembretes', component: LembretesView }
+
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
---
+++ src/views/PagamentosView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pagamentos-view">
+    <h1>Pagamentos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PagamentosView",
+};
+</script>
